5d5bb736fca917c8af9e1294222288f171707040
micalachi <60010708+micalachi@users.noreply.github.com>
2020-02-03 12:34:50 -0300

    Add files via upload
    Ejercicios de clase 1

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -1,9 +1,13 @@
-/* 
+8/* 
 	Debemos lograr tomar un dato por 'PROMPT' 
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
+var nombre;
+
+nombre= prompt ("ingrese su nombre:"); 
+
+document.getElementById("elNombre").value=nombre;
 
 }
-
