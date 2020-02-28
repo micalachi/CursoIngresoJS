3d4291f1d53d33114767162ab8aea81dbae14eb5
micalachi <60010708+micalachi@users.noreply.github.com>
2020-02-28 13:47:10 -0300

    ejercicios resueltos, preguntar al profe 9

---
+++ 1-EntradaSalida/1-EntradaSalida/jsEntradaSalida-10.js
@@ -0,0 +1,23 @@
+/*Debemos lograr tomar el importe por ID.
+Transformarlo a entero (parseInt), luego
+mostrar el importe con un Descuento del 25 %
+en el cuadro de texto "RESULTADO"*/
+function mostrarAumento()
+{
+    var importe;
+    var resultado;
+    var descuento;
+    
+    importe=document.getElementById("importe").value;
+    
+    importe=parseInt(importe);
+
+    resultado=parseInt(resultado);
+    
+    resultado=importe*25/100;
+
+    descuento=importe-resultado;
+
+    document.getElementById("resultado").value=descuento
+    
+}
---
+++ 1-EntradaSalida/1-EntradaSalida/jsEntradaSalida-3.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+    var nombre;
+
+    nombre= ("elNombre");
+    
+    nombre= document.getElementById("elNombre").value; 
+
+    alert("Mi nombre es "+ nombre);
+
+}
+
+
