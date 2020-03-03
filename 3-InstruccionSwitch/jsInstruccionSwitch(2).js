51c9b15a24f06936e6487fcdcb2c09265f7b248f
micalachi <60010708+micalachi@users.noreply.github.com>
2020-03-02 21:32:48 -0300

    listo

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,31 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes= document.getElementById('mes').value;
+switch(mes)
+{
+case"Enero":
+case"Febrero":
+case"Marzo":
+case"Abril":
+case"Mayo":
+case"Junio":
 
-alert (mesDelAño);
+alert ("Falta para el invierno");
+break;
+
+case"Julio":
+case"Agosto":
+
+alert("Abrigate que hace frío");
+break;
+
+default:
+
+alert("Ya pasamos el frío, ahora calor");
+}   
+  
+
 
 
 
